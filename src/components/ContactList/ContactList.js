3ab56e9e197b9328372366df4d8bb7bd85e2c344
VorontsovaYulia <vorontsovayulia89@gmail.com>
2023-10-02 21:34:17 +0300

    fixed loader

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -5,8 +5,6 @@ import TableBody from '@mui/material/TableBody';
 import TableContainer from '@mui/material/TableContainer';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import Box from '@mui/material/Box';
-import LinearProgress from '@mui/material/LinearProgress';
 import { ContactCard } from "components/ContactCard/ContactCard"
 import { selectContacts, selectFilter } from "redux/contacts/selectors";
 import { fetchContacts } from 'redux/contacts/operations';
@@ -16,29 +14,24 @@ export const ContactList = () => {
     const filter = useSelector(selectFilter);
     const filterContacts = contacts.filter(({ name }) => name.toLowerCase().includes(filter.toLowerCase()));
     const dispatch = useDispatch();
-    const isLoading = useSelector(state => state.contacts.isLoading)
+
     useEffect(() => {
         dispatch(fetchContacts())
     }, [dispatch]);
     
     return (
-        isLoading ? <Box sx={{ width: '100%', marginTop: 4}}>
-                <LinearProgress />
-            </Box> :
-            <div>
-                <TableContainer component={Paper}>
-                    <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                        <TableBody>
-                            {filterContacts.map(el =>
-                                <TableRow
-                                    key={el.id}
-                                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                                    <ContactCard item={el} />
-                                </TableRow>
-                            )}
-                        </TableBody>
-                    </Table>
-                </TableContainer>
-            </div>
+        <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableBody>
+                    {filterContacts.map(el =>
+                        <TableRow
+                            key={el.id}
+                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                            <ContactCard item={el} />
+                        </TableRow>
+                    )}
+                </TableBody>
+            </Table>
+        </TableContainer>
     )
 };
